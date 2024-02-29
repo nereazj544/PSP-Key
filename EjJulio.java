5aac30342ffe21773f9345d877f1b11816fb556f
Nerea ZJ <117531760+nereazj544@users.noreply.github.com>
2024-02-29 16:37:43 +0100

    Part 1 of the exercise

--- EjJulio.java
+++ EjJulio.java
@@ -13,21 +13,31 @@ import java.security.Signature;
 public class EjJulio {
 	public static void main(String[] args) throws Exception {
 		KeyStore ks = KeyStore.getInstance("pkcs12");
+		
 		char[] pwdArray = "renn".toCharArray();
+		
 		ks.load(new FileInputStream("C:\\cygwin64\\home\\nzjha\\certs\\keystore.p12"), pwdArray);
+		
 		PrivateKey priv = (PrivateKey) ks.getKey("neo", pwdArray);
-		try(BufferedInputStream in = new BufferedInputStream(new FileInputStream("C:\\Users\\nzjha\\Desktop\\Clase-eciplse\\ECIPLSE\\PSP\\pdf\\Colecciones.pdf"));
-				BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(System.getProperty("user.home") + "//Desktop//Colecciones.pdf.sing"))) {
+		
+		try (BufferedInputStream in = new BufferedInputStream(
+				new FileInputStream("C:\\Users\\nzjha\\Desktop\\Clase-eciplse\\ECIPLSE\\PSP\\pdf\\Colecciones.pdf"));
+				BufferedOutputStream out = new BufferedOutputStream(
+						new FileOutputStream(System.getProperty("user.home") + "//Desktop//Colecciones.pdf.sing"))) {
+			
 			Signature sing = Signature.getInstance("SHA512withRSA");
 			sing.initSign(priv);
-			byte [] buffer = new byte[1024];
+			
+			byte[] buffer = new byte[1024];
+			
 			int n;
-			while((n = in.read(buffer))>0) {
+			
+			while ((n = in.read(buffer)) > 0) {
 				sing.update(buffer, 0, n);
 			}
 			byte[] signature = sing.sign();
 			out.write(signature);
-	
-	}
+
 		}
+	}
 }
